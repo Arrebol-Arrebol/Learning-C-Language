4fe0f6be0c72af7379109c5576b16a036a433ddc
Arrebol-Arrebol <vicissitude.arrebol@gmail.com>
2025-10-30 20:09:20 +0800

    Class 2

---
+++ Work-10-17/Work_10_17 CLASS TWO/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//Class TWO
+
+//int main()
+//{
+//	/*printf("%zd\n", sizeof(char));
+//	printf("%zd\n", sizeof(_Bool));
+//	printf("%zd\n", sizeof(short));
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof(long));
+//	printf("%zd\n", sizeof(long long));
+//	printf("%zd\n", sizeof(float));
+//	printf("%zd\n", sizeof(double));
+//	printf("%zd\n", sizeof(long double));*/
+//
+//	/*short num1 = 2;
+//	int num2 = 10;
+//	printf("%zd\n",sizeof(num1 = num2+1));
+//	printf("%d\n", num1);*/
+//
+//
+//	return 0;
+//}
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d£¬%d", &a, &b);
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 0;
+//	scanf(" %c", &ch);
+//	printf("%c\n", ch);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int day = 0;
+//	scanf("%d%*c%d%*c%d", &year, &month, &day);
+//	printf("%d %d %d\n", year, month, day);
+//	return 0;
+//}
